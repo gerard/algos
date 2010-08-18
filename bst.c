292ee8529e20c4ac04ad0b3bdf08ad6eccb8e800
Gerard Lledo <gerard.lledo@gmail.com>
2010-08-18 12:26:56 +0300

    bst.c: Add binary search tree deletion

--- bst.c
+++ bst.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-#define N_ELEMS 100
+#include <time.h>
+#define N_ELEMS         100
+#define N_DELETES       10000
+
+struct bst_stats {
+    int total_mallocs;
+    int total_frees;
+} stats;
 
 struct node {
     int v;
@@ -22,8 +29,25 @@ int node_is_left_child(struct node *n)
     return n->parent->left == n;
 }
 
+struct node *node_sibling(struct node *n)
+{
+    if (node_is_left_child(n)) {
+        return n->parent->right;
+    } else if (node_is_right_child(n)) {
+        return n->parent->left;
+    } else {
+        return NULL;
+    }
+}
+
+int node_childs(struct node *n)
+{
+    return !!n->left + !!n->right;
+}
+
 struct node *bst_alloc(int v) {
     struct node *n = malloc(sizeof(struct node));
+    stats.total_mallocs++;
 
     n->v = v;
     n->left = n->right = n->parent = NULL;
@@ -34,6 +58,13 @@ struct node *bst_alloc(int v) {
 void bst_free(struct node *n)
 {
     free(n);
+    stats.total_frees++;
+}
+
+int bst_size(struct node *tree)
+{
+    if (tree == NULL) return 0;
+    else return bst_size(tree->left) + bst_size(tree->right) + 1;
 }
 
 struct node *bst_insert(struct node *tree, struct node *parent, int v)
@@ -85,6 +116,53 @@ struct node *bst_successor(struct node *tree, int v)
     }
 }
 
+struct node *bst_delete(struct node *tree, int v)
+{
+    struct node *n = bst_find(tree, v);
+
+    if (!n) return tree;
+
+    if (node_childs(n) == 0) {
+        if (node_is_left_child(n)) {
+            n->parent->left = NULL;
+        } else if(node_is_right_child(n)) {
+            n->parent->right = NULL;
+        } else {
+            assert(n == tree);
+            tree = NULL;
+        }
+        bst_free(n);
+        return tree;
+    }
+
+    if (node_childs(n) == 1) {
+        struct node *n_only_child = n->left ? n->left : n->right;
+
+        if (node_is_left_child(n)) {
+            n->parent->left = n_only_child;
+        } else if (node_is_right_child(n)) {
+            n->parent->right = n_only_child;
+        } else {
+            assert(n == tree);
+            tree = n_only_child;
+        }
+
+        n_only_child->parent = n->parent;
+        bst_free(n);
+        return tree;
+    }
+
+    assert(node_childs(n) == 2);
+    struct node *n_succ = bst_successor(tree, n->v);
+    assert(n_succ);
+
+    int n_succ_value = n_succ->v;
+    bst_delete(tree, n_succ_value);
+    n->v = n_succ_value;
+
+    return tree;
+}
+
 void assert_bst_properties(struct node *tree)
 {
     if (!tree) return;
@@ -102,6 +180,8 @@ void assert_bst_properties(struct node *tree)
 
 void assert_bst_successor(struct node *tree)
 {
+    if (!tree) return;
+
     struct node *snode;
     int last_min = bst_min(tree)->v;
     do {
@@ -126,7 +206,17 @@ int main(int argc, char *argv[])
         root = bst_insert(root, NULL, values[i]);
     }
 
+    assert(bst_size(root) == (stats.total_mallocs - stats.total_frees));
     assert_bst_properties(root);
     assert_bst_successor(root);
+
+    for (i = 0; i < N_DELETES; i++) {
+        root = bst_delete(root, values[random() % 100]);
+
+        assert(bst_size(root) == (stats.total_mallocs - stats.total_frees));
+        assert_bst_properties(root);
+        assert_bst_successor(root);
+    }
+
     return 0;
 }
